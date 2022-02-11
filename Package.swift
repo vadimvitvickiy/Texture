08760de0d08712ee045fe3c58f17e490324932c2
vadimvitvickiy <pluckito@gmail.com>
2022-02-11 17:39:46 +0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,29 +3,36 @@
 
 import PackageDescription
 
-let headersSearchPath: [CSetting] = [.headerSearchPath("."),
-                                     .headerSearchPath("Base"),
-                                     .headerSearchPath("Debug"),
-                                     .headerSearchPath("Details"),
-                                     .headerSearchPath("Details/Transactions"),
-                                     .headerSearchPath("Layout"),
-                                     .headerSearchPath("Private"),
-                                     .headerSearchPath("Private/Layout"),
-                                     .headerSearchPath("TextExperiment/Component"),
-                                     .headerSearchPath("TextExperiment/String"),
-                                     .headerSearchPath("TextExperiment/Utility"),
-                                     .headerSearchPath("TextKit"),
-                                     .headerSearchPath("tvOS"),]
+let headersSearchPath: [CSetting] = [
+    .headerSearchPath("."),
+    .headerSearchPath("Base"),
+    .headerSearchPath("Debug"),
+    .headerSearchPath("Details"),
+    .headerSearchPath("Details/Transactions"),
+    .headerSearchPath("Layout"),
+    .headerSearchPath("Private"),
+    .headerSearchPath("Private/Layout"),
+    .headerSearchPath("TextExperiment/Component"),
+    .headerSearchPath("TextExperiment/String"),
+    .headerSearchPath("TextExperiment/Utility"),
+    .headerSearchPath("TextKit"),
+    .headerSearchPath("tvOS"),
+]
 
 let sharedDefines: [CSetting] = [
-                                // Disable "old" textnode by default for SPM
-                                .define("AS_ENABLE_TEXTNODE", to: "0"),
-    
-                                // PINRemoteImage always available for Texture
-                                .define("AS_PIN_REMOTE_IMAGE", to: "1"),
-                                
-                                // always disabled
-                                .define("IG_LIST_COLLECTION_VIEW", to: "0"),]
+    // Disable "old" textnode by default for SPM
+    .define("AS_ENABLE_TEXTNODE", to: "0"),
+
+    // always enabled
+    .define("AS_PIN_REMOTE_IMAGE", to: "1"),
+
+    .define("AS_USE_MAPKIT", to: "1"),
+
+    .define("AS_USE_VIDEO", to: "1"),
+
+    // always disabled
+    .define("IG_LIST_COLLECTION_VIEW", to: "0"),
+]
 
 func IGListKit(enabled: Bool) -> [CSetting] {
     let state: String = enabled ? "1" : "0"
@@ -39,10 +46,10 @@ func IGListKit(enabled: Bool) -> [CSetting] {
 let package = Package(
     name: "Texture",
     platforms: [
-             .macOS(.v10_15),
-             .iOS(.v10),
-             .tvOS(.v10)
-         ],
+        .macOS(.v10_15),
+        .iOS(.v10),
+        .tvOS(.v10)
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -56,22 +63,48 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/pinterest/PINRemoteImage.git", .branch("master")),
-        .package(url: "https://github.com/3a4oT/IGListKit", .branch("spmNumber10")),
+        .package(url: "https://github.com/appintheair/IGListKit.git", .branch("master")),
     ],
     targets: [
         .target(
             name: "AsyncDisplayKit",
             dependencies: ["PINRemoteImage"],
             path: "spm/Sources/AsyncDisplayKit",
-            cSettings: headersSearchPath + sharedDefines + IGListKit(enabled: false)
+            cSettings: headersSearchPath + sharedDefines + IGListKit(enabled: false),
+            linkerSettings: [
+                .linkedFramework("MapKit"),
+                .linkedFramework("AVFoundation"),
+                .linkedFramework("CoreMedia"),
+                .linkedFramework("CoreLocation"),
+            ]
         ),
         .target(
             name: "AsyncDisplayKitIGListKit",
-            dependencies: ["IGListKit", "PINRemoteImage"],
+            dependencies: [
+                .product(name: "IGListKit", package: "IGListKit"),
+                .product(name: "IGListDiffKit", package: "IGListKit"),
+                "PINRemoteImage"
+            ],
             path: "spm/Sources/AsyncDisplayKitIGListKit/AsyncDisplayKit",
-            cSettings: headersSearchPath + sharedDefines + IGListKit(enabled: true)
+            cSettings: headersSearchPath + sharedDefines + IGListKit(enabled: true),
+            linkerSettings: [
+                .linkedFramework("MapKit"),
+                .linkedFramework("AVFoundation"),
+                .linkedFramework("CoreMedia"),
+                .linkedFramework("CoreLocation"),
+            ]
         ),
     ],
     cLanguageStandard: .c11,
     cxxLanguageStandard: .cxx11
 )
+
+for target in package.targets {
+    var settings = target.cSettings ?? []
+    settings.append(contentsOf: [
+        .define("TARGET_OS_MACOS", to: "1", .when(platforms: [.macOS])),
+        .define("TARGET_OS_IOS", to: "1", .when(platforms: [.iOS])),
+        .define("TARGET_OS_TVOS", to: "1", .when(platforms: [.tvOS]))
+    ])
+    target.cSettings = settings
+}
